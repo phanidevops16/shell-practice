4d30c2121e3a170b640b2db6bed79a432af8c7d1
phani <phanidevops16@gmail.com>
2026-01-31 21:09:42 +0530

    added variable file

---
+++ Variable.sh
@@ -0,0 +1,7 @@
+
+#!/bin/bash
+
+echo "suresh: Hello ramesh, How are you?"
+echo "ramesh: Hello suresh, I am fine"
+echo "suresh : What are you learning ramesh"
+echo "ramesh : I am learning shell. What about you?"
