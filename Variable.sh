6eb521344f65f59eb300dedb710fff44fe6cd49a
phani <phanidevops16@gmail.com>
2026-02-03 10:10:54 +0530

    new line added

--- Variable.sh
+++ Variable.sh
@@ -7,3 +7,5 @@ echo "$Person1: Hello $Person2, How are you?"
 echo "$Person2: Hello $Person1, I am fine"
 echo "$Person1 : What are you learning $Person2"
 echo "$Person2 : I am learning shell. What about you?"
+
+echo "test comment"
