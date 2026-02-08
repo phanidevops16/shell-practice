6a12b544de1dd79a1c3f0e81083ffc096e199c23
phani <phanidevops16@gmail.com>
2026-02-08 16:42:08 +0530

    new datatype code

--- Logs.sh
+++ Logs.sh
@@ -4,7 +4,7 @@ USERID=$(id -u)
 LOGS_FOLDER= "/var/log/shell-script"
 LOG_File="/var/log/shell-script/$0.log"
 
-if [$USERID -ne 0]; then
+if [ $USERID -ne 0 ]; then
    echo "Please run this script with root user access"
    exit 1
 fi 
@@ -13,7 +13,7 @@ mkdir -p $LOGS_FOLDER
 
 VALIDATE()
 {
-if [$1 -ne 0]; then
+if [ $1 -ne 0 ]; then
    echo "$2 ... Failed"
    exit 1
 
@@ -26,3 +26,5 @@ echo "Installing Nginx"
 dnf install nginx -y & >> $LOG_File
 
 VALIDATE $? "Nginx Installation"
+
+echo "Hello Installation done"
